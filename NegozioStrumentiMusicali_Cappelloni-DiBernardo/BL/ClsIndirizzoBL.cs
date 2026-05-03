faf04c56f3cb3d902a35df31a4563f806878741c
st11111_Cappelloni <st11111@iismarconipieralisi.it>
2026-05-03 10:35:00 +0200

    Classi BL e altro
    Aggiunte, con Insert e Update:
    -ClsOrdineBL
    -ClsNegozioBL
    -ClsUtenteBL
    -ClsCasaProduttriceBL
    -ClsIndirizzoBL
    
    Allineati i valori degli enum di utente a quelli nel DB
    
    Cambiamento nome da ClsCasaProdruttrice a ClsCasaProduttrice

---
+++ NegozioStrumentiMusicali_Cappelloni-DiBernardo/BL/ClsIndirizzoBL.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySqlConnector;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegozioStrumentiMusicali
+{
+    /// <summary>
+    /// Sviluppata da Leonardo Di Bernardo
+    /// </summary>
+    public static class ClsIndirizzoBL
+    {
+        /// <summary>
+        /// Inserimento di un record in indirizzi
+        /// </summary>
+        /// <param name="connection">Connessione al DB</param>
+        /// <param name="indirizzo">Record da inserire</param>
+        /// <param name="comunicazione">Comunicazione in uscita</param>
+        /// <returns>ID del nuovo record. Se -1 insert non riuscito</returns>
+        public static long InsertIndirizzo(ref MySqlConnection connection, ClsIndirizzo indirizzo, out string comunicazione)
+        {
+            //VARIABILI 
+            long _ID = -1;
+            comunicazione = String.Empty;
+
+            try
+            {
+                //Apro la connessione
+                connection.Open();
+
+                //Creo il comando DML
+                string _dml =
+                    "INSERT into indirizzi " +
+                    "(codicepostale, comune, via, nazione, essereSede, casaProduttriceID)" +
+                    "VALUES(@codicepostale, @comune, @via, @nazione, @essereSede, @casaProduttriceID)";
+
+                //Creo l'oggetto command
+                MySqlCommand _cmd = new MySqlCommand(_dml, connection);
+
+                //Inserisco i valori
+                _cmd.Parameters.AddWithValue("@codicepostale", indirizzo.CodicePostale);
+                _cmd.Parameters.AddWithValue("@comune", indirizzo.Comune);
+                _cmd.Parameters.AddWithValue("@via", indirizzo.Via);
+                _cmd.Parameters.AddWithValue("@nazione", indirizzo.Nazione);
+                _cmd.Parameters.AddWithValue("@essereSede", indirizzo.EssereSede);
+                _cmd.Parameters.AddWithValue("@casaProduttriceID", indirizzo.CasaProduttriceID);
+
+
+                //Eseguo il comando
+                int _numRec = _cmd.ExecuteNonQuery();
+                if (_numRec == 1) //1 significa che il comando è stato eseguito con successo
+                    _ID = _cmd.LastInsertedId; //Ottengo l'ID generato in automatico dal DBMS
+
+                comunicazione = "Casa produttrice inserita con successo nel DataBase";
+
+            }
+            catch (Exception ex)
+            {
+                comunicazione = ex.Message;
+            }
+            finally
+            {
+                //Chiudo la connessione
+                connection.Close();
+            }
+
+            return _ID;
+        }
+        /// <summary>
+        /// Update di un record di indirizzi
+        /// </summary>
+        /// <param name="connection">Connessione al DB</param>
+        /// <param name="indirizzo">Dati record da aggiornare</param>
+        /// <param name="comunicazione">Comunicazione in uscita</param>
+        public static void UpdateIndirizzo(ref MySqlConnection connection, ClsIndirizzo indirizzo, out string comunicazione)
+        {
+            //VARIABILI
+            comunicazione = String.Empty;
+
+            try
+            {
+                //Apro la connessione
+                connection.Open();
+
+                //Compongo il comando dml
+                string _dml =
+                    "UPDATE indirizzi SET " +
+                    "codicepostale = @codicepostale, " +
+                    "comune = @comune, " +
+                    "via = @via, " +
+                    "nazione = @nazione, " +
+                    "essereSede = @essereSede, " +
+                    "casaProdruttriceID = @casaProdruttriceID " +
+                    "WHERE ID = @ID";
+
+
+                //Creo l'oggetto command
+                MySqlCommand _cmd = new MySqlCommand(_dml, connection);
+
+                //Inserisco i valori
+                _cmd.Parameters.AddWithValue("@codicepostale", indirizzo.CodicePostale);
+                _cmd.Parameters.AddWithValue("@comune", indirizzo.Comune);
+                _cmd.Parameters.AddWithValue("@via", indirizzo.Via);
+                _cmd.Parameters.AddWithValue("@nazione", indirizzo.Nazione);
+                _cmd.Parameters.AddWithValue("@essereSede", indirizzo.EssereSede);
+                _cmd.Parameters.AddWithValue("@casaProdruttriceID", indirizzo.CasaProduttriceID);
+                _cmd.Parameters.AddWithValue("@ID", indirizzo.ID);
+
+                //Eseguo il comando
+                _cmd.ExecuteNonQuery();
+
+                comunicazione = "Indirizzo aggiornato correttamente nel DataBase";
+            }
+            catch (Exception ex)
+            {
+                comunicazione = ex.Message;
+            }
+            finally
+            {
+                //Chiudo la connessione
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ NegozioStrumentiMusicali_Cappelloni-DiBernardo/BL/ClsNegozioBL.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySqlConnector;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegozioStrumentiMusicali
+{
+    /// <summary>
+    /// Sviluppata da Leonardo Di Bernardo
+    /// </summary>
+    public static class ClsNegozioBL
+    {
+        /// <summary>
+        /// Inserimento di un record in negozi
+        /// </summary>
+        /// <param name="connection">Connessione al DB</param>
+        /// <param name="negozio">Record da inserire</param>
+        /// <param name="comunicazione">Comunicazione in uscita</param>
+        /// <returns>ID del nuovo record. Se -1 insert non riuscito</returns>
+        public static long InsertNegozio(ref MySqlConnection connection, ClsNegozio negozio, out string comunicazione)
+        {
+            //VARIABILI 
+            long _ID = -1;
+            comunicazione = String.Empty;
+
+            try
+            {
+                //Apro la connessione
+                connection.Open();
+
+                //Creo il comando DML
+                string _dml =
+                    "INSERT into negozi " +
+                    "(nome, bandito, pathimmagine, indirizzoID)" +
+                    "VALUES(@nome, @bandito, @pathimmagine, @indirizzoID)";
+
+                //Creo l'oggetto command
+                MySqlCommand _cmd = new MySqlCommand(_dml, connection);
+
+                //Inserisco i valori
+                _cmd.Parameters.AddWithValue("@nome", negozio.Nome);
+                _cmd.Parameters.AddWithValue("@bandito", negozio.Bandito);
+                _cmd.Parameters.AddWithValue("@pathimmagine", negozio.PathImmagine);
+                _cmd.Parameters.AddWithValue("@indirizzoID", negozio.IndirizzoID);
+
+
+                //Eseguo il comando
+                int _numRec = _cmd.ExecuteNonQuery();
+                if (_numRec == 1) //1 significa che il comando è stato eseguito con successo
+                    _ID = _cmd.LastInsertedId; //Ottengo l'ID generato in automatico dal DBMS
+
+                comunicazione = "Negozio inserito con successo nel DataBase";
+            }
+            catch (Exception ex)
+            {
+                comunicazione = ex.Message;
+            }
+            finally
+            {
+                //Chiudo la connessione
+                connection.Close();
+            }
+
+            return _ID;
+        }
+        /// <summary>
+        /// Update di un record di negozi
+        /// </summary>
+        /// <param name="connection">Connessione al DB</param>
+        /// <param name="negozio">Dati record da inserire</param>
+        /// <param name="comunicazione">Comunicazione in uscita</param>
+        public static void UpdateNegozio(ref MySqlConnection connection, ClsNegozio negozio, out string comunicazione)
+        {
+            //VARIABILI
+            comunicazione = String.Empty;
+
+            try
+            {
+                //Apro la connessione
+                connection.Open();
+
+                //Compongo il comando dml
+                string _dml =
+                    "UPDATE negozi " +
+                    "SET nome = @nome, " +
+                    "bandito = @bandito, " +
+                    "pathimmagine = @pathimmagine, " +
+                    "indirizzoID = @indirizzoID " +
+                    "WHERE ID = @ID";
+
+
+                //Creo l'oggetto command
+                MySqlCommand _cmd = new MySqlCommand(_dml, connection);
+
+                //Inserisco i valori
+                _cmd.Parameters.AddWithValue("@nome", negozio.Nome);
+                _cmd.Parameters.AddWithValue("@bandito", negozio.Bandito);
+                _cmd.Parameters.AddWithValue("@pathimmagine", negozio.PathImmagine);
+                _cmd.Parameters.AddWithValue("@indirizzoID", negozio.IndirizzoID);
+                _cmd.Parameters.AddWithValue("@ID", negozio.ID);
+
+
+                //Eseguo il comando
+                _cmd.ExecuteNonQuery();
+
+                comunicazione = "Negozio aggiornato correttamente nel DataBase";
+            }
+            catch (Exception ex)
+            {
+                comunicazione = ex.Message;
+            }
+            finally
+            {
+                //Chiudo la connessione
+                connection.Close();
+            }
+        }
+    }
+}
