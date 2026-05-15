21636967bdcd5890f8e67203480cb12475a514b4
st11111_Cappelloni <st11111@iismarconipieralisi.it>
2026-05-15 10:12:27 +0200

    ClassiBL
    Creazione della connessione dentro il GetAll, parametro stringa di connessione invece di connessione

--- NegozioStrumentiMusicali_Cappelloni-DiBernardo/BL/ClsStrumentoACordaBL.cs
+++ NegozioStrumentiMusicali_Cappelloni-DiBernardo/BL/ClsStrumentoACordaBL.cs
@@ -324,21 +324,22 @@ namespace NegozioStrumentiMusicali
         /// <summary>
         /// Prende tutti i record di strumentiacorda con anche le informazione della generalizzazione da strumentimusicali
         /// </summary>
-        /// <param name="connection">Connessione al DB</param>
+        /// <param name="_connection">Connessione al DB</param>
         /// <param name="ordinaPerPiuRecente">Se true, ordina per ID in maniera decrescente. Se false ordina per ID in maniera crescente</param>
         /// <param name="comunicazione">Comunicazione in uscita</param>
         /// <param name="limiteRecord">Numero massimo di record da caricare. Accetta valori da 2 in su</param>
         /// <returns>La lista con tutti i record. Se è nulla il caricamento non è andato a buon fine</returns>
-        public static List<ClsStrumentoACorda> GetAllStrumentiACorda(ref MySqlConnection connection, bool ordinaPerPiuRecente, out string comunicazione, int limiteRecord = 0)
+        public static List<ClsStrumentoACorda> GetAllStrumentiACorda(string stringaDiConnessione, bool ordinaPerPiuRecente, out string comunicazione, int limiteRecord = 0)
         {
             //VARIABILI
             comunicazione = String.Empty;
             List<ClsStrumentoACorda> _strumentiACorda = new List<ClsStrumentoACorda>();
+            MySqlConnection _connection = new MySqlConnection(stringaDiConnessione);
 
             try
             {
                 //Apro la connessione
-                connection.Open();
+                _connection.Open();
 
                 //Creo la query con la join tra strumentimusicali e strumentiacorda
                 //Abbino le righe in base a ID <-> strumentomusicaleID
@@ -377,7 +378,7 @@ namespace NegozioStrumentiMusicali
                 }
 
                 //Creo l'oggetto command
-                MySqlCommand _cmd = new MySqlCommand(_query, connection);
+                MySqlCommand _cmd = new MySqlCommand(_query, _connection);
 
                 //Inserisco il limite se richiesto
                 if (limiteRecord >= 2)
@@ -408,7 +409,7 @@ namespace NegozioStrumentiMusicali
             finally
             {
                 //Chiudo la connessione
-                connection.Close();
+                _connection.Close();
             }
 
             return _strumentiACorda;
