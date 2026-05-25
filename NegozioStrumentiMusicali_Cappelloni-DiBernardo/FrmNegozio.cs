218a6706a3546c4928d836ac9f0c5876dbc69b78
st11141-DiBernardo <st11141@iismarconipieralisi.it>
2026-05-25 14:48:55 +0200

    Dettagli negozio
    mostra i dettagli del negozio dall'ordine che si sta visualizzando

--- NegozioStrumentiMusicali_Cappelloni-DiBernardo/FrmNegozio.cs
+++ NegozioStrumentiMusicali_Cappelloni-DiBernardo/FrmNegozio.cs
@@ -14,14 +14,17 @@ namespace NegozioStrumentiMusicali
     {
         public FrmNegozio(ClsNegozio negozio)
         {
+            InitializeComponent();
+            //prendo l'indirizzo tramite l'ID del negozio
             string _comunicazione;
             ClsIndirizzo _indirizzoNegozio = ClsIndirizzoBL.GetOneIndirizzo(ref Program._connessioneAlDB, negozio.IndirizzoID, out _comunicazione);
-            InitializeComponent();
+
+
             tbNome.Text = negozio.Nome;
             nudID.Value = negozio.ID;
             tbComune.Text = _indirizzoNegozio.Comune;
             tbVia.Text = _indirizzoNegozio.Via;
-            nudCap.Text = _indirizzoNegozio.CodicePostale;
+            nudCap.Value = Convert.ToDecimal(_indirizzoNegozio.CodicePostale);
         }
 
         private void FrmNegozio_Load(object sender, EventArgs e)
